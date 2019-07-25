0a1c0cb5565e9db75f0d3476ea6ebd0163fe0167
jorge.carbajal <jorge.carbajal@dish.com.mx>
2019-07-24 19:34:29 -0500

    Rutas Hijas

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'usuario', component: UsuarioComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
